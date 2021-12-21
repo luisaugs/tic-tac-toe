51bf6ce00fcb2aaadbd8d14707c023cbce42754b
luisaugs <luis.augsburger@gmail.com>
2021-12-21 08:14:08 -0300

    add css to winner cells

--- app.js
+++ app.js
@@ -32,35 +32,59 @@ const checkBoard = (id, result) => {
     arrayResult[id] = result;
     // console.log(arrayResult)
     if ((arrayResult[0] == arrayResult[1] && arrayResult[1] == arrayResult[2]) && (arrayResult[0] && arrayResult[1] && arrayResult[2])){
-        console.log('tateti 1');
+        // console.log('tateti 1');
+        boxes[0].classList.add("box-winner");
+        boxes[1].classList.add("box-winner");
+        boxes[2].classList.add("box-winner");
         return true;
     }
     if ((arrayResult[3] == arrayResult[4] && arrayResult[4] == arrayResult[5]) && (arrayResult[3] && arrayResult[4] && arrayResult[5])){
-        console.log('tateti 2');
+        // console.log('tateti 2');
+        boxes[3].classList.add("box-winner");
+        boxes[4].classList.add("box-winner");
+        boxes[5].classList.add("box-winner");
         return true;
     }
     if ((arrayResult[6] == arrayResult[7] && arrayResult[7] == arrayResult[8]) && (arrayResult[6] && arrayResult[7] && arrayResult[8])){
-        console.log('tateti 3');
+        // console.log('tateti 3');
+        boxes[6].classList.add("box-winner");
+        boxes[7].classList.add("box-winner");
+        boxes[8].classList.add("box-winner");
         return true;
     }
     if ((arrayResult[0] == arrayResult[3] && arrayResult[3] == arrayResult[6]) && (arrayResult[0] && arrayResult[3] && arrayResult[6])){
-        console.log('tateti 4');
+        // console.log('tateti 4');
+        boxes[0].classList.add("box-winner");
+        boxes[3].classList.add("box-winner");
+        boxes[6].classList.add("box-winner");
         return true;
     }
     if ((arrayResult[1] == arrayResult[4] && arrayResult[4] == arrayResult[7]) && (arrayResult[1] && arrayResult[4] && arrayResult[7])){
-        console.log('tateti 5');
+        // console.log('tateti 5');
+        boxes[1].classList.add("box-winner");
+        boxes[4].classList.add("box-winner");
+        boxes[7].classList.add("box-winner");
         return true;
     }
     if ((arrayResult[2] == arrayResult[5] && arrayResult[5] == arrayResult[8]) && (arrayResult[2] && arrayResult[5] && arrayResult[8])){
-        console.log('tateti 6');
+        // console.log('tateti 6');
+        boxes[2].classList.add("box-winner");
+        boxes[5].classList.add("box-winner");
+        boxes[8].classList.add("box-winner");
         return true;
     }
     if ((arrayResult[0] == arrayResult[4] && arrayResult[4] == arrayResult[8]) && (arrayResult[0] && arrayResult[4] && arrayResult[8])){
-        console.log('tateti 7');
+        // console.log('tateti 7');
+        boxes[0].classList.add("box-winner");
+        boxes[4].classList.add("box-winner");
+        boxes[8].classList.add("box-winner");
         return true;
     }
     if ((arrayResult[2] == arrayResult[4] && arrayResult[4] == arrayResult[6]) && (arrayResult[2] && arrayResult[4] && arrayResult[6])){
-        console.log('tateti 8');
+        // console.log('tateti 8');
+        boxes[2].classList.add("box-winner");
+        boxes[4].classList.add("box-winner");
+        boxes[6].classList.add("box-winner");
         return true;
     }
 }
@@ -97,7 +121,6 @@ const init = (text) => {
         userInfo.innerText = '';
     }, 1000);
 }
-
 
 // About me!
 init('Thanks for use it!!! 😎');
